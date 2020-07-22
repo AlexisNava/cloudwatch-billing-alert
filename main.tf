b8c9e0843ab2b6e4d30f46d7d6f4ee133f338381
Alexis Nava <brian.alexis.nava.arreola@gmail.com>
2020-07-22 03:14:17 -0500

    Add cloudwatch_alarm module (#2)
    * Add cloudwatch-alarm module
    
    * Update the command Install the local modules in the README file
    
    * Add terraform.tfstate and terraform.tfstate.backup files to the .gitignore file
    
    * Remove unit property from cloudwatch_alarm module
    
    * Use the cloudwatch_alarm module

--- main.tf
+++ main.tf
@@ -1,5 +1,12 @@
 provider "aws" {
-    access_key = ""
-    secret_key = ""
-    region = "us-east-1"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+  region     = "us-east-1"
+}
+
+module "cloudwatch_alarm" {
+  source = "./modules/cloudwatch_alarm"
+
+  alarm_name = "Estimated Resources Cost"
+  threshold  = "60"
 }
