fa7675792ff6b79386ad424b5692c3a1d147f5be
Alexis Nava <brian.alexis.nava.arreola@gmail.com>
2020-07-30 08:33:37 -0500

    Update the authentication method for the provider (#7)

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
-  region     = "us-east-1"
+  region                  = "us-east-1"
+  shared_credentials_file = "~/.aws/credentials"
+  profile                 = "alxmedium_administrator"
 }
 
 module "billing_cloudwatch_alarm" {
--- variables.tf
+++ variables.tf
@@ -1,13 +1,3 @@
-variable "aws_access_key" {
-  description = "AWS Access Key ID"
-  type        = string
-}
-
-variable "aws_secret_key" {
-  description = "AWS Secret Access Key"
-  type        = string
-}
-
 variable "sms_enpoint" {
   description = "Phone number to subscribe to the SMS alert"
   type        = string
