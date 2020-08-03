01e81745fc64d8bcb9fdd4e80e236b8799046504
Alexis Nava <brian.alexis.nava.arreola@gmail.com>
2020-08-03 10:18:32 -0500

    Add lifecycle_rule to the versioned_bucket (#9)

--- modules/s3_versioned_bucket/outputs.tf
+++ modules/s3_versioned_bucket/outputs.tf
@@ -1,14 +1,14 @@
 output "bucket_id" {
-  description = "ID of the versioned_bucket"
-  value       = aws_s3_bucket.versioned_bucket.id
+  description = "ID of the versioned_s3_bucket"
+  value       = aws_s3_bucket.versioned_s3_bucket.id
 }
 
 output "bucket_arn" {
-  description = "ARN of the versioned_bucket"
-  value       = aws_s3_bucket.versioned_bucket.arn
+  description = "ARN of the versioned_s3_bucket"
+  value       = aws_s3_bucket.versioned_s3_bucket.arn
 }
 
 output "bucket_domain_name" {
-  description = "Bucket Domain Name of the versioned_bucket"
-  value       = aws_s3_bucket.versioned_bucket.bucket_domain_name
+  description = "Bucket Domain Name of the versioned_s3_bucket"
+  value       = aws_s3_bucket.versioned_s3_bucket.bucket_domain_name
 }
