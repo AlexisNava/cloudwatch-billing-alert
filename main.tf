8b1d77e62d6d68f66a6bbf7c1e5144598528b86a
AlexisNava <brian.alexis.nava.arreola@gmail.com>
2020-08-29 10:51:55 -0500

    Remove the use of the s3_versioned_bucket module

--- main.tf
+++ main.tf
@@ -3,22 +3,3 @@ provider "aws" {
   shared_credentials_file = "~/.aws/credentials"
   profile                 = "terraform_modules_administrator"
 }
-
-module "s3_versioned_bucket" {
-  source      = "./modules/s3_versioned_bucket"
-  bucket_name = "terraform-state-of-terraform-modules-project"
-
-  versioned_s3_bucket_tags = {
-    "MadeBy"          = "alxmedium_administrator"
-    "MadeWith"        = "Terraform"
-    "Module/Resource" = "s3_versioned_bucket"
-    "Project"         = "terraform_modules"
-  }
-
-  versioned_s3_bucket_lifecycle_rule_tags = {
-    "MadeBy"          = "alxmedium_administrator"
-    "MadeWith"        = "Terraform"
-    "Module/Resource" = "s3_versioned_bucket"
-    "Project"         = "terraform_modules"
-  }
-}
