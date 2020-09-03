aabce3e700ee62e5bcd74244cb6f3911cad8cd72
Alexis Nava <brian.alexis.nava.arreola@gmail.com>
2020-09-02 19:36:16 -0500

    feat: Add networking module (#15)

--- main.tf
+++ main.tf
@@ -3,3 +3,56 @@ provider "aws" {
   shared_credentials_file = "~/.aws/credentials"
   profile                 = "terraform_modules_administrator"
 }
+
+module "networking" {
+  source = "./modules/networking"
+  vpc_tags = {
+    "MadeBy"          = "terraform_modules_administrator"
+    "MadeWith"        = "Terraform"
+    "Module/Resource" = "networking"
+    "Project"         = "terraform_modules"
+    "Name"            = "terraform_modules_vpc"
+  }
+  private_subnet_1_tags = {
+    "MadeBy"          = "terraform_modules_administrator"
+    "MadeWith"        = "Terraform"
+    "Module/Resource" = "networking"
+    "Project"         = "terraform_modules"
+    "Name"            = "terraform_modules_private_subnet_1"
+  }
+  private_subnet_2_tags = {
+    "MadeBy"          = "terraform_modules_administrator"
+    "MadeWith"        = "Terraform"
+    "Module/Resource" = "networking"
+    "Project"         = "terraform_modules"
+    "Name"            = "terraform_modules_private_subnet_2"
+  }
+  vpc_default_network_acl_tags = {
+    "MadeBy"          = "terraform_modules_administrator"
+    "MadeWith"        = "Terraform"
+    "Module/Resource" = "networking"
+    "Project"         = "terraform_modules"
+    "Name"            = "terraform_modules_default_network_acl"
+  }
+  vpc_default_route_table_tags = {
+    "MadeBy"          = "terraform_modules_administrator"
+    "MadeWith"        = "Terraform"
+    "Module/Resource" = "networking"
+    "Project"         = "terraform_modules"
+    "Name"            = "terraform_modules_default_route_table"
+  }
+  default_route_table_internet_gateway_tags = {
+    "MadeBy"          = "terraform_modules_administrator"
+    "MadeWith"        = "Terraform"
+    "Module/Resource" = "networking"
+    "Project"         = "terraform_modules"
+    "Name"            = "terraform_modules_default_route_table_internet_gateway"
+  }
+  vpc_default_security_group_tags = {
+    "MadeBy"          = "terraform_modules_administrator"
+    "MadeWith"        = "Terraform"
+    "Module/Resource" = "networking"
+    "Project"         = "terraform_modules"
+    "Name"            = "terraform_modules_default_security_group"
+  }
+}
---
+++ modules/networking/README.md
@@ -0,0 +1,38 @@
+# networking
+
+**networking** is a Terraform module that creates networking components components 
+
+## Requirements
+
+- [Terraform >= v0.12.28](https://www.terraform.io/downloads.html)
+
+## Installation
+
+### Initialize the project
+
+```sh
+terraform init
+```
+
+### Install the local modules
+
+```sh
+terraform get
+```
+
+## Inputs
+
+| Name           | Description       | Type   | Default | Required |
+| :---:          | :---:             | :---:  | :---:   | :---:    |
+| vpc_tags | Tags for the VPC | map(string) | -       | :heavy_check_mark: |
+| private_subnet_1_tags | Tags for the private_subnet_1 | map(string) | -       | :heavy_check_mark: |
+| private_subnet_2_tags | Tags for the private_subnet_2 | map(string) | -       | :heavy_check_mark: |
+| vpc_default_network_acl_tags | Tags for the vpc_default_network_acl | map(string) | -       | :heavy_check_mark: |
+| vpc_default_route_table_tags | Tags for the vpc_default_route_table | map(string) | -       | :heavy_check_mark: |
+| default_route_table_internet_gateway_tags | Tags for the default_route_table_internet_gateway | map(string) | -       | :heavy_check_mark: |
+| vpc_default_security_group_tags | Tags for the vpc_default_security_group | map(string) | -       | :heavy_check_mark: |
+
+
+## License
+
+**networking** is licensed under [Apache License, Version 2.0](https://github.com/AlexisNava/terraform_modules/blob/master/LICENSE).
