9830bd727d446a321cbcb65362910125e09ff914
AlexisNava <brian.alexis.nava.arreola@gmail.com>
2020-09-09 21:41:27 -0500

    feat: Update ec2_instance module to receive ec2_instance_ami_id and ec2_instance_volume_tags values

--- modules/ec2_instance/main.tf
+++ modules/ec2_instance/main.tf
@@ -1,25 +1,10 @@
-data "aws_ami" "ubuntu_ami" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
-resource "aws_instance" "ubuntu_instance" {
-  ami                         = data.aws_ami.ubuntu_ami.id
+resource "aws_instance" "ec2_instance" {
   instance_type               = "t2.micro"
-  key_name                    = "terraform_modules_ec2_ubuntu_instance_key_pair"
+  key_name                    = "terraform_modules_ec2_instance_instance_key_pair"
   associate_public_ip_address = true
-  vpc_security_group_ids      = var.ubuntu_instance_security_groups
-  subnet_id                   = var.ubuntu_instance_subnet_id
-  tags                        = var.ubuntu_instance_tags
+  ami                         = var.ec2_instance_ami_id
+  vpc_security_group_ids      = var.ec2_instance_security_groups
+  subnet_id                   = var.ec2_instance_subnet_id
+  tags                        = var.ec2_instance_tags
+  volume_tags                 = var.ec2_instance_volume_tags
 }
--- modules/ec2_instance/variables.tf
+++ modules/ec2_instance/variables.tf
@@ -1,14 +1,24 @@
-variable "ubuntu_instance_security_groups" {
-  description = "List of the security groups for the ubuntu_instance"
+variable "ec2_instance_security_groups" {
+  description = "List of the security groups for the ec2_instance"
   type        = list(string)
 }
 
-variable "ubuntu_instance_subnet_id" {
-  description = "Subnet Id for the ubuntu_instance"
+variable "ec2_instance_subnet_id" {
+  description = "Subnet Id for the ec2_instance"
   type        = string
 }
 
-variable "ubuntu_instance_tags" {
-  description = "Tags for ubuntu_instance_tags"
+variable "ec2_instance_tags" {
+  description = "Tags for ec2_instance"
   type        = map(string)
+}
+
+variable "ec2_instance_volume_tags" {
+  description = "Tags for ec2_instance_volume_tags"
+  type        = map(string)
+}
+
+variable "ec2_instance_ami_id" {
+  description = "AMI Id for the ec2_instance"
+  type        = string
 }
